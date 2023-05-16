6240cfc4c6fb2b69a38fc923d453fc876aa94492
Abenathi201 <abenathi.sindaphi201@gmail.com>
2023-05-16 10:57:44 +0200

    Finished some exercises

---
+++ index.js
@@ -0,0 +1,14 @@
+function myName () {
+    alert("Hello my name is Abenathi!!!");
+};
+
+function firstFunction () {
+    document.getElementById("text").innerHTML= "Text changed";
+};
+// I'm loggin a name in the console
+console.log("Hello World");
+var sum = 2+2;
+
+console.log(sum);
+alert("This is the browser's alert box");
+
